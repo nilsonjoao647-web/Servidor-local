20af3a598b3e5d7e49efce9db179373731d8ac60
Ismael pereira <nilsonjoao647@gmail.com>
2026-05-06 12:22:23 -0100

    minhas alterações

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,14 @@ import swaggerUi from "swagger-ui-express"
 import { resolvers, typeDefs } from "./graphq/index.js"
 import { ApolloServer } from "@apollo/server"
 import { expressMiddleware } from "@as-integrations/express5"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: " http://localhost:3000",
+    credentials: true
+}))
 
 app.use("/service", serviceRouter)
 app.use("/orcamento", orcamentoRouter)
